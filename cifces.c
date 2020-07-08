b21bb7e7c5b2529f25e8b779d6c5d7680510de99
Royer23 <51539000+Royer23@users.noreply.github.com>
2020-07-07 20:17:25 -0400

    Create cifces.c
    primer ejercicio del primer parcial INF-317

---
+++ cifces.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include <omp.h>
+
+void main()
+{
+  char nombre[25]="ROYER CESAR ALANIS FLORES";
+  int i=0;
+  #pragma omp critical  
+  for (i=0; i<25; i++){
+    printf("%c",nombre[i]);
+  }
+  printf("\n");
+  
+  int idomp=0;
+  //segun el numero de procesadores se escoge para hacer el cifrado de CESAR
+  while(idomp==0){
+    #pragma omp parallel
+    idomp = omp_get_thread_num();   
+  }
+
+
+
+  #pragma omp critical
+  {
+  for(i=0; i<25; i++){
+    if(nombre[i]!=32){
+      if((nombre[i]+idomp)>90){
+        nombre[i]=(64+(nombre[i]+idomp)%90);  
+      }
+      else{
+        nombre[i]=nombre[i]+idomp;
+      }
+    }   
+  }
+  }
+  for (i=0; i<25; i++){
+    printf("%c",nombre[i]);
+  }
+  printf("\n");
+  printf("rotacion para descifrar: %d \n",idomp);
+  
+}
