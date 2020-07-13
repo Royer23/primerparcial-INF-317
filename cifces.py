c2430122e4b6a71891ef6ccfbb876a9857906279
Royer23 <51539000+Royer23@users.noreply.github.com>
2020-07-12 20:46:39 -0400

    Update cifces.py

--- cifces.py
+++ cifces.py
@@ -1,12 +1,19 @@
+# -*- coding: utf-8 -*-
 """
 Created on Sat Jul 11 22:59:52 2020
 
 @author: Royer Cesar Alanis Flores
+ALANIS FLORES ROYER CESAR
+8449437 LP
+INF-317
+PRIMER PARCIAL EJERCICIO 2
+CIFRADO DE CESAR EN PYTHON
 """
 
 
 cif="ROYER CESAR ALANIS FLORES"
 l=len(cif)
+print(cif)
 ces=""
 for i in range(l):
     if(cif[i]==' '):
